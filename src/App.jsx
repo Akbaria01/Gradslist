b45339cba23ad30c847516c2b588bfec84c49118
clariza2002 <124947047+clariza2002@users.noreply.github.com>
2025-11-19 22:12:42 -0500

    Created the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#eaecef]">
       <Header />
-      <main className="flex-1 px-4 py-6 max-w-7xl mx-auto w-full">
+      <main className="flex-1 w-full px-0 py-0">
         <Outlet />
       </main>
       <Footer />
